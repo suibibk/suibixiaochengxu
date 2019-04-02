f2d8e73242859bcefc39b708d669a13cb1152af1
suibibk <suibibk@qq.com>
2019-04-02 13:16:30 +0800

    首页样式

--- pages/sb/sb.js
+++ pages/sb/sb.js
@@ -3,7 +3,46 @@ var url = "https://www.suibibk.com";
 Page({
   data: {
     motto: 'Hello World',
+    topics: [],
     new: 1
+  },
+  getTopics: function (page,type) {
+    wx.request({
+      url: url + "/getTopics",
+      data: {
+        menuid: '0',
+        userid: '0',
+        page: page,
+        hot: '',
+        type: type
+      },
+      method: "POST",
+      header: {
+        //"Content-Type":"application/json"
+        "Content-Type": " application/x-www-form-urlencoded"
+
+      },
+      //success: function(res) {
+      success: res => {
+        console.log(res.data)
+        var topics = res.data.obj;
+        for (var index in topics) {
+          topics[index].description= topics[index].description.replace(/\/fileupload\/images/g, url + "/fileupload/images/");
+        }
+        this.setData({
+          topics: topics
+        });
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getTopics(1,0);
   },
   //事件处理函数
   getNewTopics: function () {
@@ -14,28 +53,7 @@ Page({
       user: 0,
       menu: 0
     });
-     wx.request({
-       url: url+"/getTopics",
-        data: {
-          menuid: '0',
-          userid: '0',
-          page: '1',
-          hot : '',
-          type: '0'
-        },
-        method:"POST",
-        header: {
-          //"Content-Type":"application/json"
-          "Content-Type": " application/x-www-form-urlencoded"
-          
-        },
-        success: function (res) {
-          console.log(res.data)
-        },
-        fail: function (err) {
-          console.log(err)
-        }
-      })
+    this.getTopics(1, 0);
   },
   //事件处理函数
   getMonthTopics: function () {
@@ -46,6 +64,7 @@ Page({
       user: 0,
       menu: 0
     });
+    this.getTopics(1, 2);
   },
   //事件处理函数
   getYearTopics: function () {
@@ -56,6 +75,7 @@ Page({
       user: 0,
       menu: 0
     });
+    this.getTopics(1, 3);
   },
   //事件处理函数
   getHotUsers: function () {
